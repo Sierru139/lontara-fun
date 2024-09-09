26a54537bddf95eaf4842b2d173452d5da9a548d
Sierru 139 <humanoidgg@gmail.com>
2024-09-09 14:43:24 +0800

    first Comm

---
+++ lontar.js
@@ -0,0 +1,6 @@
+const ᨆᨔᨘᨀᨙ = document.getElementById('ᨆᨔᨘᨀᨙ');
+function ᨆᨔᨘᨀᨂ() {
+    const ᨕᨗᨔᨗ = ᨆᨔᨘᨀᨙ.value;
+    const ᨖᨔᨗᨒᨙ = document.getElementById('ᨖᨔᨗᨒᨙ');
+    ᨖᨔᨗᨒᨙ.innerHTML = ᨕᨗᨔᨗ;
+}
